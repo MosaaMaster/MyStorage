a5c26ac6ae5364dc43f1ef7f8ae1bd6ecf28f933
ahmad sabah <96618078+itahmad@users.noreply.github.com>
2022-07-29 22:59:00 +0300

    Update MoneyBox.vb

--- نظام ادارة المبيعات/MoneyBox.vb
+++ نظام ادارة المبيعات/MoneyBox.vb
@@ -11,6 +11,9 @@
             ComboBox2.Items.Add("ذمم عملاء")
             ComboBox2.Items.Add("ذمم شركاء")
             ComboBox2.Items.Add("مصاريف استيراد")
+                        ComboBox2.Items.Add("مصاريف استيراد")
+
+            
         End If
 
     End Sub
